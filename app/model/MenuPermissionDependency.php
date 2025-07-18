633de6404100d7320fba8d6b6a940d221b317685
xidear <xidear@126.com>
2025-07-18 09:41:08 +0800

    增加关联

--- app/model/MenuPermissionDependency.php
+++ app/model/MenuPermissionDependency.php
@@ -9,4 +9,16 @@ class MenuPermissionDependency extends BasePivot
     protected $pk = 'dependency_id';
 
     protected string $table = 'menu_permission_dependency';
+
+
+
+    public function menu(): \think\model\relation\BelongsTo
+    {
+        return $this->belongsTo(Menu::class,'menu_id','menu_id');
+    }
+    public function permission(): \think\model\relation\BelongsTo{
+        return $this->belongsTo(Permission::class,'permission_id','permission_id');
+    }
+
+
 }
