bd65a967e8786ffddd3fa7b6b68dec55c56b3ddd
xidear <xidear@126.com>
2025-07-18 09:49:11 +0800

    增加关联

--- app/model/Role.php
+++ app/model/Role.php
@@ -1,14 +1,47 @@
 <?php
+
 namespace app\model;
 
 use app\common\BaseModel;
+use think\model\relation\BelongsToMany;
+use think\model\relation\HasMany;
 
 class Role extends BaseModel
 {
     protected $pk = 'role_id';
 
-    public function permission(){
-        return $this->belongsToMany('permission','role_permission','permission_id','role_id');
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany('permission', RolePermission::class, 'permission_id', 'role_id');
+    }
+
+    public function admins(): BelongsToMany
+    {
+        return $this->belongsToMany('admin', AdminRole::class, 'admin_id', 'role_id');
+    }
+
+    public function menus(): BelongsToMany
+    {
+        return $this->belongsToMany('menu', RoleMenu::class, 'menu_id', 'role_id');
+    }
+
+    public function role_menus(): \think\model\relation\HasMany
+    {
+        return $this->hasMany(RoleMenu::class, 'role_id', 'role_id');
+    }
+
+
+    public function roleMenus(): \think\model\relation\HasMany
+    {
+        return $this->hasMany(RoleMenu::class, 'role_id', 'role_id');
+    }
+
+    public function role_permissions(): HasMany{
+        return $this->hasMany(RolePermission::class, 'role_id', 'role_id');
+    }
+
+    public function rolePermissions(): HasMany{
+        return $this->hasMany(RolePermission::class, 'role_id', 'role_id');
     }
 
 }
